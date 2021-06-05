77967dcea560ca1d7acee0e1f9197079feb86122
0118Add <60157610+0118Add@users.noreply.github.com>
2021-06-05 23:57:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,11 +18,11 @@ sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 #添加额外软件包
-rm -rf package/lean/luci-app-dockerman
-git clone https://github.com/lisaac/luci-lib-docker package/lean/luci-lib-docker
-git clone https://github.com/lisaac/luci-app-dockerman.git package/lean/luci-app-dockerman
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/docker package/lean/docker
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/dockerd package/lean/dockerd
+#rm -rf package/lean/luci-app-dockerman
+git clone https://github.com/0118Add/sirpdboy-package.git package/sirpdboy-package
+#git clone https://github.com/lisaac/luci-app-dockerman.git package/lean/luci-app-dockerman
+#svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/docker package/lean/docker
+#svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/dockerd package/lean/dockerd
 #rm -rf package/lean/v2ray-plugin
 #svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin package/v2ray-plugin
 #svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns-le package/smartdns-le
