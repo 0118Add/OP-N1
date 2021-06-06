2b49588cb51d7a1c1584d930aac344291d24dcf9
0118Add <60157610+0118Add@users.noreply.github.com>
2021-06-06 16:52:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages.git;openwrt-18.06' feeds.conf.default
