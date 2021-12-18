07d09f9a704568f5d19d05067722414da84a6fd8
0118Add <60157610+0118Add@users.noreply.github.com>
2021-12-18 20:59:10 +0800

    Update respawn_mks.sh

--- scripts/respawn_mks.sh
+++ scripts/respawn_mks.sh
@@ -20,16 +20,3 @@ find ./*_plus.sh ./*_plusplus.sh  -maxdepth 1 -path "*" | xargs -i sed -i 's/TGT
 
 
 echo "mk_files respawned."
-
-cd ..
-#diff -uN openwrt/files/update-amlogic-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-amlogic-openwrt.sh > update-amlogic-openwrt.sh.patch
-#diff -uN openwrt/files/update-amlogic-openwrt-old.sh mk_openwrt_src_20210401/openwrt/files/update-amlogic-openwrt-old.sh > update-amlogic-openwrt-old.sh.patch
-#diff -uN openwrt/files/update-beikeyun-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-beikeyun-openwrt.sh > update-beikeyun-openwrt.sh.patch
-#diff -uN openwrt/files/update-vplus-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-vplus-openwrt.sh  > update-vplus-openwrt.sh.patch
-#diff -uN openwrt/files/update-l1pro-openwrt.sh mk_openwrt_src_20210401/openwrt/files/update-l1pro-openwrt.sh  > update-l1pro-openwrt.sh.patch
-patch -p1 < update-amlogic-openwrt-old.sh.patch
-patch -p1 < update-amlogic-openwrt.sh.patch
-patch -p1 < update-beikeyun-openwrt.sh.patch
-patch -p1 < update-vplus-openwrt.sh.patch
-#patch -p1 < update-l1pro-openwrt.sh.patch
-echo "patching done."
