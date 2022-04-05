4d84e3f60327197f98e97b781e60314d8ad78ec9
0118Add <60157610+0118Add@users.noreply.github.com>
2022-04-05 14:48:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -135,3 +135,6 @@ wget -O ./package/base-files/files/etc/banner https://raw.githubusercontent.com/
 #修改bypass的makefile
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+
+./scripts/feeds update -a
+./scripts/feeds install -a
